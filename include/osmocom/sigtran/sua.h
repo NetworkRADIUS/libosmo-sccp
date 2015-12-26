7ec6687debc11329100b095880ccd9de891ee194
Harald Welte <laforge@gnumonks.org>
2015-12-26 23:34:03 +0100

    allow user to give private data to osmo_sua_user_create()
    ... which can be resolved from the prim_cb() by calling
    osmo_sua_link_get_user_priv().

--- include/osmocom/sigtran/sua.h
+++ include/osmocom/sigtran/sua.h
@@ -8,7 +8,7 @@ struct osmo_sua_link;
 
 void osmo_sua_set_log_area(int area);
 
-struct osmo_sua_user *osmo_sua_user_create(void *ctx, osmo_prim_cb prim_cb);
+struct osmo_sua_user *osmo_sua_user_create(void *ctx, osmo_prim_cb prim_cb, void *priv);
 void osmo_sua_user_destroy(struct osmo_sua_user *user);
 
 int osmo_sua_server_listen(struct osmo_sua_user *user, const char *hostname, uint16_t port);
@@ -19,3 +19,4 @@ struct osmo_sua_link *osmo_sua_client_get_link(struct osmo_sua_user *user);
 /* user hands us a SCCP-USER SAP primitive down into the stack */
 int osmo_sua_user_link_down(struct osmo_sua_link *link, struct osmo_prim_hdr *oph);
 
+void *osmo_sua_link_get_user_priv(struct osmo_sua_link *slink);
